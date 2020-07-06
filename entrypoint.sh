f8265d6cd5e237ad4eb4351d8657bf39f518838a
vsoch <vsochat@stanford.edu>
2020-07-06 13:20:07 -0600

    adding folder input option, not yet tested
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,35 @@ fi
 printf "Files in workspace:\n"
 ls
 
-# Check that the paper markdown file exists
-if [ ! -f "${INPUT_PAPER_MARKDOWN}" ]; then
-    printf "Input markdown for paper ${INPUT_PAPER_MARKDOWN} not found.\n"
+# If both paper markdown and paper_dir are unset, nothing to do
+if [ -z "${INPUT_PAPER_MARKDOWN}" ] && [ -z "${INPUT_PAPER_DIR}" ]; then
+    printf "Please define either paper_markdown or paper_dir.\n"
     exit 1
+fi
+
+# Check that the paper markdown file exists
+if [ ! -z "${INPUT_PAPER_MARKDOWN}" ]; then
+    if [ ! -f "${INPUT_PAPER_MARKDOWN}" ]; then
+        printf "Input markdown for paper ${INPUT_PAPER_MARKDOWN} not found.\n"
+        exit 1
+    fi
+fi
+
+if [ ! -z "${INPUT_PAPER_DIR}" ]; then
+
+    # Make sure input directoy exists
+    if [ ! -d "${INPUT_PAPER_DIR}" ]; then
+        printf "Input directory ${INPUT_PAPER_DIR} not found.\n"
+        exit 1
+    fi
+
+    # If output directory defined, it needs to exist too
+    if [ ! -z "${INPUT_OUTPUT_DIR}" ]; then
+        if [ ! -d "${INPUT_OUTPUT_DIR}" ]; then
+            printf "Output directory ${INPUT_OUTPUT_DIR} not found.\n"
+            exit 1
+        fi
+    fi
 fi
 
 # And the latex template
@@ -52,6 +77,7 @@ fi
 
 # Alert the user to all their settings
 
+printf "paperdir: ${INPUT_PAPER_DIR}\n"
 printf "template: ${INPUT_LATEX_TEMPLATE}\n"
 printf "  output: ${INPUT_PAPER_OUTFILE}\n"
 printf "  bibtex: ${INPUT_BIBTEX}\n"
@@ -62,9 +88,13 @@ printf "    logo: ${INPUT_PNG_LOGO}\n"
 
 # Generate ---------------------------------------------------------------------
 
-if [ "${INPUT_PDF_TYPE}" == "minimal" ]; then
+generate_minimal() {
 
-    printf "Producing minimal pdf.\n"
+    INPUT_PAPER_MARKDOWN="${1}"
+    INPUT_PAPER_OUTFILE="${2}"
+    INPUT_BIBTEX="${3}"
+    printf "Producing minimal pdf for ${INPUT_PAPER_MARKDOWN} -> ${INPUT_PAPER_OUTFILE}\n"
+
     COMMAND="pandoc ${INPUT_PAPER_MARKDOWN} --filter pandoc-citeproc"
     if [ ! -z "${INPUT_BIBTEX}" ]; then
         COMMAND="${COMMAND} --bibliography ${INPUT_BIBTEX}"
@@ -72,6 +102,25 @@ if [ "${INPUT_PDF_TYPE}" == "minimal" ]; then
     COMMAND="${COMMAND} -o ${INPUT_PAPER_OUTFILE}"
     printf "${COMMAND}\n"
     $COMMAND
+
+}
+
+# Minimal PDF generation
+
+if [ "${INPUT_PDF_TYPE}" == "minimal" ]; then
+    if [ ! -z "${INPUT_PAPER_MARKDOWN}" ]; then
+        generate_minimal "${INPUT_PAPER_MARKDOWN}" "${INPUT_PAPER_OUTFILE}" "${INPUT_BIBTEX}"
+    else
+        for INPUT_PAPER_MARKDOWN in $(find "${INPUT_PAPER_DIR}" -regex '.*/[^/]*.md'); do
+            outfile=$(basename ${INPUT_PAPER_MARKDOWN%.md}).pdf
+            if [ ! -z "${INPUT_OUTPUT_DIR}" ]; then
+                outfile="${INPUT_OUTPUT_DIR}"/"${outfile}"
+            fi 
+            generate_minimal "${INPUT_PAPER_MARKDOWN}" "${outfile}" "${INPUT_BIBTEX}"
+        done
+    fi
+
+# PDF with template generation
 
 else
 
@@ -122,19 +171,37 @@ else
     if [ ! -z "${INPUT_BIBTEX}" ]; then
         COMMAND="${COMMAND} --bibliography ${INPUT_BIBTEX}"
     fi
-    COMMAND="${COMMAND} -V graphics=\"true\" -V logo_path=\"${INPUT_PNG_LOGO}\" -V geometry:margin=1in --verbose -o ${INPUT_PAPER_OUTFILE} --pdf-engine=xelatex --filter /usr/bin/pandoc-citeproc ${INPUT_PAPER_MARKDOWN} --from markdown+autolink_bare_uris --template ${INPUT_LATEX_TEMPLATE}"
-    printf "$COMMAND\n"
-    printf "${COMMAND}" > pandoc_run.sh
-    chmod +x pandoc_run.sh
-    /bin/bash pandoc_run.sh
 
+    if [ ! -z "${INPUT_PAPER_MARKDOWN}" ]; then
+
+        COMMAND="${COMMAND} -V graphics=\"true\" -V logo_path=\"${INPUT_PNG_LOGO}\" -V geometry:margin=1in --verbose -o ${INPUT_PAPER_OUTFILE} --pdf-engine=xelatex --filter /usr/bin/pandoc-citeproc ${INPUT_PAPER_MARKDOWN} --from markdown+autolink_bare_uris --template ${INPUT_LATEX_TEMPLATE}"
+        printf "$COMMAND\n"
+        printf "${COMMAND}" > pandoc_run.sh
+        chmod +x pandoc_run.sh
+        /bin/bash pandoc_run.sh
+
+    else
+        for INPUT_PAPER_MARKDOWN in $(find "${INPUT_PAPER_DIR}" -regex '.*/[^/]*.md'); do
+            outfile=$(basename ${INPUT_PAPER_MARKDOWN%.md}).pdf
+            if [ ! -z "${INPUT_OUTPUT_DIR}" ]; then
+                outfile="${INPUT_OUTPUT_DIR}"/"${outfile}"
+            fi 
+            COMMAND="${COMMAND} -V graphics=\"true\" -V logo_path=\"${INPUT_PNG_LOGO}\" -V geometry:margin=1in --verbose -o ${outfile} --pdf-engine=xelatex --filter /usr/bin/pandoc-citeproc ${INPUT_PAPER_MARKDOWN} --from markdown+autolink_bare_uris --template ${INPUT_LATEX_TEMPLATE}"
+            printf "$COMMAND\n"
+            printf "${COMMAND}" > pandoc_run.sh
+            chmod +x pandoc_run.sh
+            /bin/bash pandoc_run.sh
+        done
+    fi
 fi
 
-if [ ! -f "${INPUT_PAPER_OUTFILE}" ]; then
-    printf "There was an issue rendering ${INPUT_PAPER_OUTFILE}\n"
-    exit 1
-else
+if [ -f "${INPUT_PAPER_OUTFILE}" ]; then
     printf "Files in workspace:\n"
     ls
     chmod 0777 "${INPUT_PAPER_OUTFILE}"
+fi
+if [ -f "${INPUT_PAPER_DIR}" ]; then
+    printf "Files generated:\n"
+    ls "${INPUT_PAPER_DIR}"
+    chmod -R 0777 "${INPUT_PAPER_DIR}"
 fi
